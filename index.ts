5d465ab337be66d5e56da2aa58cdcb882f41137f
Mikhail Malyshev <mikhail.malyshev@pm.bet>
2022-04-05 18:21:21 +0300

    Fix sendTx, update types

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+import type { TransactionReceipt } from '@ethersproject/providers'
+
 import WalletProvider from './WalletProvider'
 
 export { WalletContext } from './WalletContext'
 export { shortenAddress, isValidAddress } from './utils'
+
 export default WalletProvider
+
+export type { TransactionReceipt }
