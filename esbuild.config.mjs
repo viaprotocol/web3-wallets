0e31c033e48865acd5b27769936e94273d34b9a6
Aleksey Lisun <insuline.ru@gmail.com>
2022-07-14 11:33:16 +0800

    🎨 fix: linter errors

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,13 +1,6 @@
-import esbuild from 'esbuild';
+import esbuild from 'esbuild'
 // Automatically exclude all node_modules from the bundled version
-import { nodeExternalsPlugin } from 'esbuild-node-externals';
-import path from 'path';
-
-function tsPathResolver(content) {
-  const relativePath = path.relative(path.dirname(this.resourcePath), path.resolve(__dirname, '../src'));
-
-  return content.replaceAll(`from "@/`, `from "${relativePath ? relativePath + '/' : './'}`);
-}
+import { nodeExternalsPlugin } from 'esbuild-node-externals'
 
 esbuild
   .build({
@@ -30,9 +23,9 @@ esbuild
     splitting: true,
     format: 'esm',
     inject: ['esbuild.shim.js'],
-    plugins: [nodeExternalsPlugin()],
+    plugins: [nodeExternalsPlugin()]
   })
   .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    console.error(err)
+    process.exit(1)
+  })
