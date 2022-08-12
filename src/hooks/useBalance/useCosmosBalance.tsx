d29c41917b2b7d8eaf3db85ab4c5a624c985db0c
Max Korolev <nrjshka@gmail.com>
2022-08-12 09:32:36 +0100

    [frd-661] fixes

--- src/hooks/useBalance/useCosmosBalance.tsx
+++ src/hooks/useBalance/useCosmosBalance.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { StargateClient } from '@cosmjs/stargate'
 import type { TUseBalanceOptions } from './types'
 import { isCosmosWallet } from '@/utils/wallet'
