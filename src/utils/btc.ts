d430dc6bdf0cedea2c8ab2e4fb0dd44d5f78f253
Max Korolev <nrjshka@gmail.com>
2022-09-12 19:49:47 +0400

    [frd-824] fixes

--- src/utils/btc.ts
+++ src/utils/btc.ts
@@ -4,9 +4,7 @@ import { getConnectedWallets } from './common'
 import type { XDeFi } from '@/provider'
 
 const getBTCConnectedWallets = async (xDeFiProvider: XDeFi) => {
-  return await getConnectedWallets(btcChainWalletMap, async ({ name }: TChainWallet) => {
-    return await xDeFiProvider.getProviderByKey(name as any).getAccounts()
-  })
+  return await getConnectedWallets(btcChainWalletMap, async ({ name }: TChainWallet) => xDeFiProvider.getProviderByKey(name as any).getAccounts())
 }
 
 export { getBTCConnectedWallets }
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,4 +1,4 @@
-import { BTC_WALLETS_CONFIG, COSMOS_WALLETS_CONFIG, EVM_WALLETS_CONFIG, SOL_WALLETS_CONFIG, isBTCChain, isCosmosChain, isEvmChain, isSolChain } from '..'
+import { BTC_WALLETS_CONFIG, COSMOS_WALLETS_CONFIG, EVM_WALLETS_CONFIG, SOL_WALLETS_CONFIG, isBTClikeChain, isCosmosChain, isEvmChain, isSolChain } from '..'
 import type { TBTCWalletStore, TCosmosWalletStore, TEvmWalletStore, TSolWalletStore, TWalletStore } from '@/types'
 
 const isEvmWallet = (walletData: TWalletStore, fromChainId?: number): walletData is TEvmWalletStore => {
@@ -14,7 +14,7 @@ const isCosmosWallet = (walletData: TWalletStore, fromChainId?: number): walletD
 }
 
 const isBTClikeWallet = (walletData: TWalletStore, fromChainId?: number): walletData is TBTCWalletStore => {
-  return BTC_WALLETS_CONFIG.includes(walletData.name as any) && (!fromChainId || isBTCChain(fromChainId))
+  return BTC_WALLETS_CONFIG.includes(walletData.name as any) && (!fromChainId || isBTClikeChain(fromChainId))
 }
 
 export { isEvmWallet, isSolWallet, isCosmosWallet, isBTClikeWallet }
