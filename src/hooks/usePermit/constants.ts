9dfeecf75f9c240487738f5590c5501f34ed0cc0
Roman Rakhlin <rrakhlin@gmail.com>
2022-12-09 23:18:54 +0200

    added other tokens that confirm Permit

--- src/hooks/usePermit/constants.ts
+++ src/hooks/usePermit/constants.ts
@@ -21,6 +21,30 @@ const ERC2612_TOKENS: TPermitToken[] = [
   {
     address: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
     chainId: NETWORK_IDS.Polygon
+  },
+  {
+    address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0x111111111117dc0aa78b770fa6a738034120c302', // 1INCH
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9', // AAVE
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0x92D6C1e31e14520e676a687F0a93788B716BEff5', // DYDX
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0', // wstETH
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72', // ENS
+    chainId: NETWORK_IDS.Ethereum
   }
 ]
 
@@ -33,6 +57,14 @@ const DAI_TOKENS: TPermitToken[] = [
     address: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
     chainId: NETWORK_IDS.Polygon,
     noncesFn: '0x2d0335ab'
+  },
+  {
+    address: '0x7DD9c5Cba05E151C895FDe1CF355C9A1D5DA6429', // GLM
+    chainId: NETWORK_IDS.Ethereum
+  },
+  {
+    address: '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215', // CHAI
+    chainId: NETWORK_IDS.Ethereum
   }
 ]
 
