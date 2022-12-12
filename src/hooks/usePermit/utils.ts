7c344d9cef6cfdacff65289ff2048de4a4a293f3
Aleksey Lisun <insuline.ru@gmail.com>
2022-12-12 16:35:09 +0300

    🔇 fix: remove console.logs

--- src/hooks/usePermit/utils.ts
+++ src/hooks/usePermit/utils.ts
@@ -12,7 +12,6 @@ const getDomain = (permitToken: TPermitToken): TDomain => {
   const { address, chainId, name } = permitToken
 
   const domain: TDomain = chainId === NETWORK_IDS.Ethereum ? { name, version: '1', chainId, verifyingContract: address } : { name, version: '1', verifyingContract: address, salt: hexZeroPad(BigNumber.from(chainId).toHexString(), 32) }
-  console.log({ domain })
   return domain
 }
 
