39d5c0e5e1db2248ef613ef266d453aba1963f8d
Aleksey Lisun <insuline.ru@gmail.com>
2023-03-01 19:42:57 +0700

    Update persister key for web3-wallets.store in localStoragePersister

--- src/context/QueryProvider.tsx
+++ src/context/QueryProvider.tsx
@@ -13,14 +13,14 @@ const queryClient = /* #__PURE__ */ new QueryClient({
       cacheTime: 1000 * 60 * 60 * 24 * 7, // 1 week
       staleTime: 1000 * 60 * 60 * 2 // 2 hours
     }
-  }
+  },
 })
 
-const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage })
+const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage, key: 'web3-wallets.store' })
 
 persistQueryClient({
   queryClient,
-  persister: localStoragePersister
+  persister: localStoragePersister,
 })
 
 function QueryProvider({ children }: TQueryProviderProps) {
